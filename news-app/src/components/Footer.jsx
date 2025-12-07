ba6861e8fab51d34fea57583bde3164abbf29bcd
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-07 19:40:49 +0100

    Footer styled

--- news-app/src/components/Footer.jsx
+++ news-app/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer className="text-center p-2 bg-blue-600 text-white">
-      <p>&copy; {new Date().getFullYear()} Global News. All rights reserved.</p>
+      <p>
+        &copy; {new Date().getFullYear()} Nigeria News. All rights reserved.
+      </p>
     </footer>
   );
 }
