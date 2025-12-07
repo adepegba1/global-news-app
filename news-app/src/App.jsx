b155974964aa88e3751bb5b70684174f438ccc1b
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-07 19:42:38 +0100

    newscard component added for testing

--- news-app/src/App.jsx
+++ news-app/src/App.jsx
@@ -1,11 +1,9 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import NewsCard from "./components/NewsCard";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Footer></Footer>
+      <NewsCard></NewsCard>
     </>
   );
 }
