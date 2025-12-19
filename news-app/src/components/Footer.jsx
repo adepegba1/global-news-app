f21b88bdeb350aa853e87cf4c46851fe88de9c6a
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-19 14:46:05 +0100

    Name added to the footer

--- news-app/src/components/Footer.jsx
+++ news-app/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="text-center p-2 bg-blue-600 text-white">
       <p>
-        &copy; {new Date().getFullYear()} Nigeria News. All rights reserved.
+        &copy; {new Date().getFullYear()} Adepegba David. All rights reserved.
       </p>
     </footer>
   );
