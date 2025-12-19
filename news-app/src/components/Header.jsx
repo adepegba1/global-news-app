3c9458371e14c92b3e485ce15038b727bd9b74ef
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-19 14:51:31 +0100

    modify the header component to work with search component

--- news-app/src/components/Header.jsx
+++ news-app/src/components/Header.jsx
@@ -1,29 +1,28 @@
 import FormattedDate from "./FormattedDate";
-function Header() {
+import SearchBar from "./SearchBar";
+
+function Header({ onSearch, country, date, loading }) {
   return (
     <>
       <FormattedDate />
-      <header className="flex items-center justify-around bg-gray-400">
+
+      <header className="flex flex-col md:flex-row items-center justify-around bg-gray-400 p-4 gap-4">
         <img
-          className="w-20 h-20"
+          className="w-20 h-20 md:w-32 md:h-32 object-cover rounded-full border-4 border-white"
           src="https://www.shutterstock.com/shutterstock/photos/2467118167/display_1500/stock-vector-latest-news-breaking-report-breaking-news-badge-template-set-latest-hot-news-label-message-2467118167.jpg"
           alt="Logo Image"
         />
-        <h1 className="font-bold text-6xl text-blue-200 family-arial">
-          Top News
+
+        <h1 className=" family-arial font-bold text-3xl md:text-6xl text-blue-200">
+          News Reader
         </h1>
-        <div className="flex gap-2">
-          <input
-            className="border rounded p-2 bg-white"
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Search for news"
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+
+        <div className="w-full md:w-auto">
+          <SearchBar
+            onSearch={onSearch}
+            country={country}
+            date={date}
+            loading={loading}
           />
         </div>
       </header>
